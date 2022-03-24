b5e994ba479fa2a51cc66eae56aec85485f22bd7
NunezTania <tnunez@bluewin.ch>
2022-03-24 12:30:23 +0100

    feat statique clean: début d'implémentation de nettoyage. Actuellement le code ne fait qu'effacer par récursion tous les fichiers contenu dans le dossier ./site/<path>/build/

--- src/main/java/ch/heig/statique/Commands/Clean.java
+++ src/main/java/ch/heig/statique/Commands/Clean.java
@@ -2,6 +2,8 @@ package ch.heig.statique.Commands;
 
 import picocli.CommandLine;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -12,8 +14,27 @@ import java.util.concurrent.Callable;
 )
 public class Clean implements Callable<Integer> {
 
+    @CommandLine.Parameters(index = "0", description = "The file whose checksum to calculate.")
+    String file;
+
+    void deleteDirectoryRecursionJava6(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] entries = file.listFiles();
+            if (entries != null) {
+                for (File entry : entries) {
+                    deleteDirectoryRecursionJava6(entry);
+                }
+            }
+        }
+        if (!file.delete()) {
+            throw new IOException("Failed to delete " + file);
+        }
+    }
+
     @Override
     public Integer call() throws Exception {
+        File f = new File(System.getProperty("user.dir")+ "\\site" + file + "\\build");
+        deleteDirectoryRecursionJava6(f);
         return 0;
     }
 }
