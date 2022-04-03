72e6347a6e2c156eee84e82d5ac9513c25a0fd8e
NunezTania <tnunez@bluewin.ch>
2022-04-03 17:10:51 +0200

    feat: l'utilisateur peut choisir de mettre un path absolu ou non. Dans le der nier cas, le site est initialisé à l'endroit d'execution de la commande dans un dossier site. le chemin est affiché pour que les fichiers puissent être retro uvés. La commande enrichit également les fichiers comme dans les exemple fournits dans le sprint

---
+++ site/mon/site/index.md
@@ -0,0 +1,7 @@
+---titre: Mon premier article
+auteur: John Doe
+date: 2022-04-03
+---
+# Mon premier article
+## Mon sous-titre
+Le contenu de mon article.
--- src/main/java/ch/heig/statique/Commands/Init.java
+++ src/main/java/ch/heig/statique/Commands/Init.java
@@ -1,10 +1,11 @@
 package ch.heig.statique.Commands;
 
 import picocli.CommandLine;
-
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.concurrent.Callable;
+import org.apache.commons.io.FileUtils;
+import java.time.*;
 
 @CommandLine.Command(
         name = "init",
@@ -14,33 +15,49 @@ import java.util.concurrent.Callable;
 )
 public class Init implements Callable<Integer> {
 
+    static String CRLF = "\r\n";
+
     @CommandLine.Parameters(index = "0", description = "The directory where the site will be initiated")
     private File file;
 
     @Override
     public Integer call() throws Exception {
         try {
+            if (!file.isAbsolute()) // Si l'utilisateur ne donne pas un chemin absolu, la commande crée le site dans un
+                                    // nouveau fichier site à partir de l'endroit ou est executée la commande
+                // récupère le path depuis lequel la commande est executée
+                file = new File(Paths.get("").toAbsolutePath() + "\\site" + file.toString());
 
-
-            if (file.isAbsolute()) // Si l'utilisateur donne un chemin absolu, lance une exception puisque nous
-                // souhaitons créer le site statique à un endroit spécifique
-                throw new RuntimeException("You must provide a relative path for the command");
-            file = new File(Paths.get("").toAbsolutePath() + "\\site" + file.toString());
             if (!file.mkdirs()) {
                 throw new RuntimeException("Could not create directory: " + file.toString());
             }
+
+            System.out.println("Created directory: " + file.getAbsolutePath());
 
             File config = new File(file.toString() + "\\config.yaml");
             File index = new File(file.toString() + "\\index.md");
 
             if (config.createNewFile()) {
                 System.out.println("Config file created");
+                String content = "titre: titre du site" + CRLF
+                               + "description: lorem ipsum" + CRLF
+                               + "domain: example.com" + CRLF;
+                FileUtils.writeStringToFile(config, content, "UTF-8");
             } else {
                 System.out.println("Config file could not be created");
             }
 
             if (index.createNewFile()) {
                 System.out.println("Index file created");
+                String content = "---" + CRLF
+                               + "titre: Mon premier article" + CRLF
+                               + "auteur: John Doe" + CRLF
+                               + "date: " + java.time.LocalDate.now() + CRLF
+                               + "---" + CRLF
+                               + "# Mon premier article" + CRLF
+                               + "## Mon sous-titre" + CRLF
+                               + "Le contenu de mon article." + CRLF;
+                FileUtils.writeStringToFile(index, content, "UTF-8");
             } else {
                 System.out.println("Index file could not be created");
             }
