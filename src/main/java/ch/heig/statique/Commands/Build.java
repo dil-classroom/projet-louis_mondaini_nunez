2fdc456a05d8424d19cc287cbc7c4fefe330b472
Damiano <damiano.mondaini@gmail.com>
2022-04-04 08:39:59 +0200

    feat [WIP] Copy the requested dir

--- src/main/java/ch/heig/statique/Commands/Build.java
+++ src/main/java/ch/heig/statique/Commands/Build.java
@@ -1,6 +1,10 @@
 package ch.heig.statique.Commands;
 
+import ch.heig.statique.Utils.Utils;
+import org.apache.commons.io.FileUtils;
 import picocli.CommandLine;
+
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -10,9 +14,18 @@ import java.util.concurrent.Callable;
         description = "Build a static site"
 )
 public class Build implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", description = "The directory containing the site")
+    private File dir;
 
     @Override
     public Integer call() throws Exception {
+        dir = new File(System.getProperty("user.dir") + "\\site" + dir.toString());
+        Utils.copyDirectory(dir.getPath(), System.getProperty("user.dir") + "\\build");
+
+        var it = FileUtils.iterateFiles(dir, null, true);
+
+
+
         return 0;
     }
 }
--- src/main/java/ch/heig/statique/Utils/Utils.java
+++ src/main/java/ch/heig/statique/Utils/Utils.java
@@ -1,5 +1,10 @@
 package ch.heig.statique.Utils;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Utils {
     /**
      * https://mkyong.com/java/how-to-get-file-extension-in-java/
@@ -13,5 +18,14 @@ public class Utils {
         }
 
         return extension;
+    }
+
+    /**
+     * https://www.baeldung.com/java-copy-directory
+     */
+    public static void copyDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation) throws IOException {
+        File sourceDirectory = new File(sourceDirectoryLocation);
+        File destinationDirectory = new File(destinationDirectoryLocation);
+        FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
     }
 }
