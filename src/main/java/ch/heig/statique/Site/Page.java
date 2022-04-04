1d9da0bb3a181bfbc681e7893e8089e810b6fdfa
Damiano <damiano.mondaini@gmail.com>
2022-04-04 08:58:07 +0200

    feat [FIX] Documented Page.java

--- src/main/java/ch/heig/statique/Site/Page.java
+++ src/main/java/ch/heig/statique/Site/Page.java
@@ -3,6 +3,9 @@ package ch.heig.statique.Site;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Representation of a web page with an HTML content and metadata
+ */
 public class Page {
     private final Map<String, List<String>> metadata;
     private final String html;
@@ -12,10 +15,18 @@ public class Page {
         this.html = html;
     }
 
+    /**
+     * Metadata getter
+     * @return metadata
+     */
     public Map<String, List<String>> getMetadata() {
         return metadata;
     }
 
+    /**
+     * HTML getter
+     * @return HTML
+     */
     public String getHtml() {
         return html;
     }
