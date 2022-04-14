9b6ff830fa9e2adbde6b075aef4772f38b1f4756
Damiano <damiano.mondaini@gmail.com>
2022-04-14 13:42:45 +0200

    feat [WIP] Configuring the HtmlRenderer to swap .md to .html in links

--- src/main/java/ch/heig/statique/Parser/PageParser.java
+++ src/main/java/ch/heig/statique/Parser/PageParser.java
@@ -5,8 +5,12 @@ import ch.heig.statique.Utils.Utils;
 import org.commonmark.Extension;
 import org.commonmark.ext.front.matter.YamlFrontMatterExtension;
 import org.commonmark.ext.front.matter.YamlFrontMatterVisitor;
+import org.commonmark.node.Link;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.AttributeProvider;
+import org.commonmark.renderer.html.AttributeProviderContext;
+import org.commonmark.renderer.html.AttributeProviderFactory;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.io.*;
@@ -50,6 +54,19 @@ public class PageParser {
         document.accept(visitor);
 
         HtmlRenderer renderer = HtmlRenderer.builder()
+                .attributeProviderFactory(new AttributeProviderFactory() {
+                    @Override
+                    public AttributeProvider create(AttributeProviderContext context) {
+                        return new AttributeProvider() {
+                            @Override
+                            public void setAttributes(Node node, String tagName, Map<String, String> attributes) {
+                                if (node instanceof Link) {
+                                    System.out.println(node);
+                                }
+                            }
+                        };
+                    }
+                })
                 .extensions(extensions)
                 .build();
 
