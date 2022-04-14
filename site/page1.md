2f8fc103644f9d1e5f5889fd5aced459e94a23d6
Damiano <damiano.mondaini@gmail.com>
2022-04-14 12:36:45 +0200

    feat [TOREVIEW] Building simple site without handling nicely links to other md pages

---
+++ site/page1.md
@@ -0,0 +1,8 @@
+---
+titre: Mon premier article
+auteur: Bertil Chapuis
+date: 2021-03-10
+---
+# Page 1
+
+Page 1
--- src/main/java/ch/heig/statique/Commands/Build.java
+++ src/main/java/ch/heig/statique/Commands/Build.java
@@ -1,10 +1,16 @@
 package ch.heig.statique.Commands;
 
-import ch.heig.statique.Utils.Utils;
+import ch.heig.statique.Parser.PageParser;
+import ch.heig.statique.Site.Page;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import picocli.CommandLine;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -15,16 +21,44 @@ import java.util.concurrent.Callable;
 )
 public class Build implements Callable<Integer> {
     @CommandLine.Parameters(index = "0", description = "The directory containing the site")
-    private File dir;
+    private File file;
 
     @Override
-    public Integer call() throws Exception {
-        dir = new File(System.getProperty("user.dir") + "\\site" + dir.toString());
-        Utils.copyDirectory(dir.getPath(), System.getProperty("user.dir") + "\\build");
+    public Integer call() throws IOException {
+        if (file.isAbsolute())  {
+            file = new File(file.toString() + "\\site");
+        } else {
+            file = new File(Paths.get("").toAbsolutePath() + "\\" + file.toString() + "\\site");
+        }
 
-        var it = FileUtils.iterateFiles(dir, null, true);
+        File config = new File(file + "\\config.yaml");
+        if (!FileUtils.directoryContains(file, config)) {
+            throw new RuntimeException("Could not find any config.yaml file");
+        }
 
+        File build = new File(file + "\\build");
+        FileUtils.deleteDirectory(build);
 
+        FileUtils.copyDirectory(file, build, pathname -> {
+            if (pathname.getName().equals("build") && pathname.isDirectory()
+                || pathname.getName().equals("config.yaml")) {
+                return false;
+            }
+
+            return true;
+        });
+
+        var it = FileUtils.iterateFiles(build, new String[]{"md"}, true);
+
+        while (it.hasNext()) {
+            File md = it.next();
+            Page page = PageParser.parseFromMarkdownFile(md);
+            FileUtils.writeStringToFile(md, page.getHtml(), StandardCharsets.UTF_8, false);
+            if (!md.renameTo(new File(FilenameUtils.removeExtension(md.getAbsolutePath()) + ".html"))
+            ) {
+                throw new RuntimeException("Cannot rename the md files to html files");
+            }
+        }
 
         return 0;
     }
