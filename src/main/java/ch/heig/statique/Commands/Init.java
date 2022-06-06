2ef9a8b7fd078dd72303f714b610dcae6020af07
Damiano <damiano.mondaini@gmail.com>
2022-06-06 18:39:11 +0200

    feature cmd "Init" copies a template folder instead of creating files

--- src/main/java/ch/heig/statique/Commands/Init.java
+++ src/main/java/ch/heig/statique/Commands/Init.java
@@ -2,6 +2,7 @@ package ch.heig.statique.Commands;
 
 import ch.heig.statique.Utils.Utils;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import org.apache.commons.io.FileUtils;
 import picocli.CommandLine;
@@ -17,8 +18,6 @@ import picocli.CommandLine;
         description = "Init a static site")
 public class Init implements Callable<Integer> {
 
-    static String CRLF = "\r\n";
-
     @CommandLine.Parameters(
             index = "0",
             description =
@@ -28,111 +27,27 @@ public class Init implements Callable<Integer> {
     @Override
     public Integer call() {
         try {
-            if (file.isAbsolute()) {
-                file = new File(file.toString() + Utils.SEPARATOR + "site");
+            if (!file.isAbsolute()) {
+                throw new RuntimeException("Please give an absolute path");
             } else {
-                throw new RuntimeException("Please use an absolute path");
+                file = new File(file + Utils.SEPARATOR + "site");
+                if (!file.mkdirs()) {
+                    throw new RuntimeException("Failed to create a new folder");
+                }
             }
 
-            if (!file.mkdirs()) {
-                throw new RuntimeException("Could not create directory: " + file.toString());
-            }
-
-            // Affichage du chemin ou a été initialisé le site
-            System.out.println("Created directory: " + file.getAbsolutePath());
-
-            File config = new File(file.toString() + Utils.SEPARATOR + "config.yaml");
-            File index = new File(file.toString() + Utils.SEPARATOR + "index.md");
-
-            // Création du fichier de configuration
-            if (config.createNewFile()) {
-                System.out.println("Config file created");
-                String content =
-                        "titre: titre du site"
-                                + CRLF
-                                + "description: lorem ipsum"
-                                + CRLF
-                                + "domain: example.com"
-                                + CRLF;
-                FileUtils.writeStringToFile(config, content, "UTF-8");
-            } else {
-                System.out.println("Config file could not be created");
-            }
-
-            // Création du fichier index
-            if (index.createNewFile()) {
-                System.out.println("Index file created");
-                String content =
-                        "titre: Mon premier article"
-                                + CRLF
-                                + "auteur: John Doe"
-                                + CRLF
-                                + "date: "
-                                + java.time.LocalDate.now()
-                                + CRLF
-                                + "---"
-                                + CRLF
-                                + "# Mon premier article"
-                                + CRLF
-                                + "## Mon sous-titre"
-                                + CRLF
-                                + "Le contenu de mon article."
-                                + CRLF;
-                FileUtils.writeStringToFile(index, content, "UTF-8");
-            } else {
-                System.out.println("Index file could not be created");
-            }
-
-            File templateFolder = new File(file.toString() + Utils.SEPARATOR + "template");
-            File menuTemplate = new File(templateFolder + Utils.SEPARATOR + "menu.html");
-            File layoutTemplate = new File(templateFolder + Utils.SEPARATOR + "layout.html");
-
-            if (templateFolder.mkdirs()) {
-                System.out.println("Template folder created");
-            } else {
-                System.out.println("Template folder could not be created");
-            }
-
-            if (menuTemplate.createNewFile()) {
-                System.out.println("Menu template file created");
-                String content = "<ul></ul>";
-                FileUtils.writeStringToFile(menuTemplate, content, "UTF-8");
-            } else {
-                System.err.println("Menu template file could not be created");
-            }
-
-            if (layoutTemplate.createNewFile()) {
-                System.out.println("Layout template file created");
-                String content =
-                        "<!DOCTYPE html>"
-                                + CRLF
-                                + "<html>"
-                                + CRLF
-                                + "<head>"
-                                + CRLF
-                                + "<meta charset=\"UTF-8\">"
-                                + CRLF
-                                + "<title>{{ site.titre }} | {{ page.titre }}</title>"
-                                + CRLF
-                                + "</head>"
-                                + CRLF
-                                + "<body>"
-                                + CRLF
-                                + "{% include menu.html }"
-                                + CRLF
-                                + "{{ content }}"
-                                + CRLF
-                                + "</body>"
-                                + CRLF
-                                + "</html>"
-                                + CRLF;
-                FileUtils.writeStringToFile(layoutTemplate, content, "UTF-8");
-            } else {
-                System.err.println("Layout template file could not be created");
-            }
-
+            FileUtils.copyDirectory(
+                    new File(
+                            Path.of("").toAbsolutePath()
+                                    + Utils.SEPARATOR
+                                    + "src"
+                                    + Utils.SEPARATOR
+                                    + "archtemplate"
+                                    + Utils.SEPARATOR
+                                    + "site"),
+                    file);
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            System.out.println(e.getMessage());
         }
         return 0;
     }
